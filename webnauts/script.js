1a56838b28276f6166438a6314b774ad19dfde2b
SamiraRadzhabova <samiraradzabova2002@gmail.com>
2022-12-15 13:04:03 +0200

    slider

--- webnauts/script.js
+++ webnauts/script.js
@@ -1,25 +1,45 @@
-let nextBtn = document.querySelector(".comment-btn.next");
-let prevBtn = document.querySelector(".comment-btn.prev");
-let slides = document.querySelectorAll(".comment-wrapper");
-let activeSlide = document.querySelector(".active");
-let activeIndex=parseInt(activeSlide.dataset.id);
-let nextIndex = activeIndex + 1;
+// let nextBtn = document.querySelector(".comment-btn.next");
+// let prevBtn = document.querySelector(".comment-btn.prev");
+// let slides = document.querySelectorAll(".comment-wrapper");
+// let activeSlide = document.querySelector(".active");
+// let activeIndex=parseInt(activeSlide.dataset.id);
+// let nextIndex = activeIndex + 1;
 // console.log(typeof activeIndex);
 // slides.forEach((slide, i, arr)=> {
 //     console.log(slide, i, arr)
 // })
+// nextBtn.addEventListener("click", () => {
+//     if(nextIndex >= slides.length) {
+
+//         activeIndex=0
+//         nextIndex=1
+//     }
+//     console.log(nextIndex, activeIndex, slides.length);
+//     document.querySelector(".active").classList.remove("active");
+//     slides[nextIndex].classList.add("active")
+//     activeIndex= nextIndex;
+//     nextIndex++;
+// });
+// prevBtn.addEventListener("click", () => {
+//   console.log("click prev");
+// });
+let nextBtn = document.querySelector(".comment-btn.next");
+let prevBtn = document.querySelector(".comment-btn.prev");
+let slides = document.querySelectorAll(".comment-wrapper");
+let activeSlide = document.querySelector(".active");
+let nextIndex = parseInt(activeSlide.dataset.id) + 1;
 nextBtn.addEventListener("click", () => {
-    // if(nextIndex >= slides.length) {
-        
-    //     activeIndex=0
-    //     nextIndex=1
-    // }
-    console.log(nextIndex, activeIndex, slides.length);
-    document.querySelector(".active").classList.remove("active");
-    slides[nextIndex].classList.add("active")
-    activeIndex= nextIndex;
-    nextIndex++;
+  if (nextIndex >= slides.length) {
+    nextIndex = 0;
+  }
+  document.querySelector(".active").classList.remove("active");
+  slides[nextIndex].classList.add("active");
+  nextIndex++;
 });
 prevBtn.addEventListener("click", () => {
-  console.log("click prev");
+  if (nextIndex<1) {
+    nextIndex = slides.length;
+  }
+  document.querySelector(".active").classList.remove("active");
+  slides[--nextIndex].classList.add("active");
 });
